3e2a045f52388eb99724138c928945a69e29c907
Yakup <yakup2000002@gmail.com>
2025-06-01 00:24:47 +0500

    configured_docker

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('DATABASE_HOST', 'postgres'),
+        'PORT': env('DATABASE_PORT', 5432),
     }
 }
 
