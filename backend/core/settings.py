ba6aa705f6927999569247d78b9908c7e5bbb675
Yakup <yakup2000002@gmail.com>
2025-06-05 22:17:08 +0500

    added_cors_headers

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -46,7 +46,10 @@ INSTALLED_APPS = [
     'drf_spectacular',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
