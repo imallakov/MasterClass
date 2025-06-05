af9bf7060a837344305d8c23329920b524028fb1
Yakup <yakup2000002@gmail.com>
2025-06-06 04:52:19 +0500

    specified_media_root_folder

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -34,8 +35,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
-# Application definition
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
