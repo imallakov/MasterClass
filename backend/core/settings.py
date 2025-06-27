881df53c93c19b7aac8803ab98f39e2a07da0446
Yakup <yakup2000002@gmail.com>
2025-06-27 21:48:47 +0300

    token_blacklisting

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -198,7 +198,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
